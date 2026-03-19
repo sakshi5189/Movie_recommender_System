733341a544379021727fe1d7fa6eec63e744b3ee
SAKSHI SWAMI <swamisakshi128@gmail.com>
2026-03-19 20:00:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,78 @@
 import streamlit as st
-import pickle
+import pandas as pd
+import ast
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Load data
-movies = pickle.load(open('movies.pkl', 'rb'))
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+st.title('🎬 Movie Recommender System')
 
-# Recommendation function
+# Load dataset
+movies = pd.read_csv("movies.csv")
+
+# ----------- FUNCTIONS -----------
+
+def convert(obj):
+    L = []
+    try:
+        for i in ast.literal_eval(obj):
+            L.append(i['name'])
+    except:
+        pass
+    return L
+
+def convert3(obj):
+    L = []
+    count = 0
+    try:
+        for i in ast.literal_eval(obj):
+            if count != 3:
+                L.append(i['name'])
+                count += 1
+            else:
+                break
+    except:
+        pass
+    return L
+
+def fetch_director(obj):
+    L = []
+    try:
+        for i in ast.literal_eval(obj):
+            if i['job'] == 'Director':
+                L.append(i['name'])
+                break
+    except:
+        pass
+    return L
+
+# ----------- PREPROCESSING -----------
+
+movies = movies[['title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+movies.dropna(inplace=True)
+
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+movies['cast'] = movies['cast'].apply(convert3)
+movies['crew'] = movies['crew'].apply(fetch_director)
+
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
+
+# Combine all features
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+
+# Convert list → string
+movies['tags'] = movies['tags'].apply(lambda x: " ".join(x))
+
+# ----------- VECTOR + SIMILARITY -----------
+
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(movies['tags']).toarray()
+
+similarity = cosine_similarity(vectors)
+
+# ----------- RECOMMEND FUNCTION -----------
+
 def recommend(movie):
-    recommended_movies = []
-
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
 
@@ -16,25 +80,17 @@ def recommend(movie):
                         reverse=True,
                         key=lambda x: x[1])[1:6]
 
-    for i in movie_list:
-        recommended_movies.append(movies.iloc[i[0]].title)
+    return [movies.iloc[i[0]].title for i in movie_list]
 
-    return recommended_movies
-
-
-# UI
-st.title('🎬 Movie Recommender System')
+# ----------- UI -----------
 
 movie_list = movies['title'].values
 
-selected_movie_name = st.selectbox(
-    'Select a movie',
-    movie_list
-)
+selected_movie_name = st.selectbox('Select a movie', movie_list)
 
-# Button
 if st.button('Recommend'):
     recommendations = recommend(selected_movie_name)
 
+    st.subheader("🎥 Recommended Movies:")
     for movie in recommendations:
         st.write(movie)
